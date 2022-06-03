7184d93c1debcc5f888c65ac54fada953b491e8a
Valle Deniz Diaz <vallukis@gmail.com>
2022-06-03 13:52:18 +0100

    feacture: crear primera clase

---
+++ src/DocInformatico.java
@@ -0,0 +1,10 @@
+public class DocInformatico {
+
+        String nombre;
+        int tamaño;
+
+        public DocInformatico(String nombre, int tamaño) {
+            this.nombre = nombre;
+            this.tamaño = tamaño;
+        }
+}
